410a9976dc1dcd71c146c33fdd7fd575d8d2565b
paul guichon <paul.guichon@iedparis8.net>
2015-02-12 15:19:38 +0100

    mise à jour de la gestion des dom tom

--- xadmin/views/edit.py
+++ xadmin/views/edit.py
@@ -85,7 +85,6 @@ class ModelFormAdminView(ModelAdminView):
         # a field in admin.
         if isinstance(db_field, models.ManyToManyField) and not db_field.rel.through._meta.auto_created:
             return None
-
         attrs = self.get_field_attrs(db_field, **kwargs)
         return db_field.formfield(**dict(attrs, **kwargs))
 
